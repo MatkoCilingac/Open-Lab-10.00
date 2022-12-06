6eeb86a0c05e4a9f7acdeb2c1701262773d142df
MatkoCilingac <ceniga16@gmail.com>
2022-12-06 09:10:46 +0100

    set values to all variables in class book into object LOTR. Than call method vypis to write all values of the variables in object LOTR

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -8,8 +8,13 @@ namespace Open_Lab_10._00
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.title = "Lord Of The Rings";
-            Console.WriteLine(LOTR.title);
+            LOTR.Category("Roman");
+            LOTR.Title("Lord Of The Rings");
+            LOTR.Releasedate("30.10.1989");
+            LOTR.Author("Michal Bernat");
+            LOTR.Pages(10000);
+            LOTR.Vypis();
+            
         }
     }
 }
