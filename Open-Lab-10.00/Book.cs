7a61c547591d0685a7592e399c2e4a916d8e47d4
MatkoCilingac <ceniga16@gmail.com>
2022-12-06 09:01:35 +0100

    create variables in class book and then create methods

--- Open-Lab-10.00/Book.cs
+++ Open-Lab-10.00/Book.cs
@@ -6,10 +6,43 @@ namespace Open_Lab_10._00
 {
     internal class Book
     {
-        public string title;
-        private int pages;
-
         
-    
+        private string category; 
+        private string title;
+        private int pages;
+        private string releasedate;
+        private string author;    
+        public string Category(string xcategory)
+        {
+            this.category = xcategory;
+            return category;
+        }
+        public string Title(string xtitle)
+        {
+            this.title = xtitle;
+            return title;
+        }
+        public string Releasedate(string xreleasedate)
+        {
+            this.releasedate = xreleasedate;
+            return releasedate;
+        }
+        public string Author (string xauthor)
+        {
+            this.author = xauthor;
+            return author;
+        }
+        public int Pages(int xpages)
+        {
+            this.pages = xpages;
+            return pages;
+        }
+        public void Vypis()
+        {
+            Console.WriteLine(category , title , releasedate , author , pages);
+           
+
+        }
+
     }
 }
